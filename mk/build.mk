85f31953414cf8e91f4e0739b090780476b20dca
Michael Fero <6863207+mikefero@users.noreply.github.com>
2025-03-28 22:41:54 -0400

    fix: correct build and dev targets for proper LDFLAGS

--- mk/build.mk
+++ mk/build.mk
@@ -17,12 +17,13 @@ $(error "'go' is not installed or available in PATH")
 endif
 
 APP_VERSION ?= $(shell cat $(APP_DIR)/version)
-APP_COMMIT ?= $(shell git rev-parse --short HEAD 2> /dev/null)
+APP_GIT_DIRTY ?= $(shell git status --porcelain 2>/dev/null | wc -l | awk '{print $$1}')
+APP_COMMIT ?= $(shell if [ "$(APP_GIT_DIRTY)" = "0" ]; then git rev-parse --short HEAD 2>/dev/null; else echo "dev"; fi)
 APP_OS_ARCH ?= $(shell go version | awk '{print $$4;}')
 APP_GO_VERSION ?= $(shell go version | awk '{print $$3;}')
 APP_DATE_FORMAT := +'%Y-%m-%dT%H:%M:%SZ'
 APP_BUILD_DATE ?= $(shell date $(APP_DATE_FORMAT))
-define APP_LDFLAGS
+define APP_LDFLAGS_BUILD
 -X $(APP_PACKAGE).AppName=$(APP_NAME) \
 -X $(APP_PACKAGE).Version=$(APP_VERSION) \
 -X $(APP_PACKAGE).Commit=$(if $(APP_COMMIT),$(APP_COMMIT),dev) \
@@ -33,5 +34,5 @@ endef
 
 .PHONY: build
 build: ## Build the application
-	@CGO_ENABLED=0 go build -ldflags "$(APP_LDFLAGS)" \
+	@CGO_ENABLED=0 go build -ldflags "$(APP_LDFLAGS_BUILD)" \
 		-o "$(APP_DIR)/bin/$(APP_NAME)" "$(APP_DIR)"
--- mk/dev.mk
+++ mk/dev.mk
@@ -2,19 +2,19 @@
 # Development tooling
 # --------------------------------------------------
 
-define APP_LDFLAGS
+define APP_LDFLAGS_DEV
 -X $(APP_PACKAGE).AppName=$(APP_NAME) \
--X $(APP_PACKAGE).Version=dev \
+-X $(APP_PACKAGE).Version=$(APP_VERSION) \
 -X $(APP_PACKAGE).Commit=dev \
--X $(APP_PACKAGE).OsArch=dev \
--X $(APP_PACKAGE).GoVersion=dev \
--X $(APP_PACKAGE).BuildDate=dev
+-X $(APP_PACKAGE).OsArch=$(APP_OS_ARCH) \
+-X $(APP_PACKAGE).GoVersion=$(APP_GO_VERSION) \
+-X $(APP_PACKAGE).BuildDate=$(APP_BUILD_DATE)
 endef
 
 .PHONY: version
 version: ## Run the version command
-	@CGO_ENABLED=0 go run -ldflags "$(APP_LDFLAGS)" "$(APP_DIR)" version
+	@CGO_ENABLED=0 go run -ldflags "$(APP_LDFLAGS_DEV)" "$(APP_DIR)" version
 
 .PHONY: license
 license: ## Run the license command
-	@CGO_ENABLED=0 go run -ldflags "$(APP_LDFLAGS)" "$(APP_DIR)" license
+	@CGO_ENABLED=0 go run -ldflags "$(APP_LDFLAGS_DEV)" "$(APP_DIR)" license
